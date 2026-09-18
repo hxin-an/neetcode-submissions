b42179a7f63c688a4a2637ecd04be3b2727418c2
黃信安 <61717816+hxin-an@users.noreply.github.com>
2026-09-18 17:35:28 +0800

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        map <int,int> mp;
+        int size = stones.size();
+
+        for (auto a : stones)
+            mp[a]++;
+        while(size>1){
+            auto iter = mp.rbegin();
+            int big = iter->first;
+            iter->second--;
+
+            if(iter->second == 0){
+                mp.erase(big);
+                iter = mp.rbegin();
+            }
+
+            int smaller = iter->first;
+            iter->second--;
+            if(iter->second == 0)
+                mp.erase(smaller);
+
+            if(big != smaller){
+                mp[big-smaller]++;
+                size++;
+            }
+            size -= 2;
+        }
+        if (mp.empty())
+            return 0;
+
+        return mp.begin()->first;
+
+    }
+};
